d94fc7f93752809b2705e90c22ac1a60fa99628d
ProfMara <94469709+ProfMara@users.noreply.github.com>
2023-02-06 20:05:22 -0300

    Update kwitterPage.js

--- kwitterPage.js
+++ kwitterPage.js
@@ -10,3 +10,54 @@ const firebaseConfig = {
     measurementId: "G-FDGY8V42QV"
   };   
   
+firebase.initializeApp(firebaseConfig);  
+//FACILITAR A SUA VIDA
+var db = firebase.database();
+
+//pegar os valores do localStorage
+var nomeSala = localStorage.getItem("roomName");
+var nome = localStorage.getItem("userName");
+
+function enviar(){
+  var msg = document.getElementById("msg").value;
+  db.ref(nomeSala).push({
+    msg:msg,
+    like:0,
+    nome:nome
+  })
+  document.getElementById("msg").value = '';
+}
+
+//ler do banco de dados as mensagens e mostrar na tela
+db.ref(nomeSala).on("value", (data)=>{
+  //limpa a div
+  document.getElementById("output").innerHTML = '';
+  //repete comando para cada valor
+  data.forEach((dados)=>{
+    if(dados.key != 'proposito'){
+      var id = dados.key;
+      var valores = dados.val();
+
+      var mensagem = valores['msg'];
+      var nome = valores['nome'];
+      var likes = valores['like'];
+      //faz uma tag html para a mensagem
+      var msghtml  =  "<h4>"+ mensagem +"</h4>";
+      //faz uma tag html para o nome
+      var nomehtml = "<h4>"+nome+"</h4>";
+      //faz um botão para os likes
+      var botaoLike = "<button class='btn btn-warning' id="+id+" value="+likes+" onclick='attLike(this.id)'>";
+      var likeshtml = "<span class='glyphicon glyphicon-thumbs-up'></span>Likes:"+likes+"</button>";
+      linha = msghtml + nomehtml + botaoLike + likeshtml;
+      document.getElementById("output").innerHTML += linha
+    }
+  })
+})
+
+function attLike(id){
+  likes = document.getElementById(id).value;
+  likes = Number(likes) + 1;
+  db.ref(nomeSala).child(id).update({
+    like:likes
+  })
+}
